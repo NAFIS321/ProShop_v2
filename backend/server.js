60985a79182d17b9956fba8f59e63eb051129f27
Nafis Ahammed <nafisahammed769@gmail.com>
2026-07-21 14:48:51 +0600

    do it

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Cookie parser Middleware
 app.use(cookieParser());
-    app.get('/', (req, res) => {
-      res.send('API is running...');
-    });
+  
 
 
 app.use('/api/products', productRoutes);
@@ -44,8 +42,27 @@ app.get('/api/config/paypal', (req, res) =>
  app.use(
    '/uploads',
    express.static(path.join(__dirname, '/uploads'))
+
   );
 
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, '/fronted/build')));
+  //This is for express 5 code but in my package.json I inastall express 4
+  // Any route that is not API → send index.html
+  // app.get('*', (req, res) =>
+  //   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  // );
+//------------------------this is express 4 code ------------------
+app.use((req, res) => {
+    res.sendFile(path.resolve(__dirname, '../fronted/build/index.html'));
+});
+//------------------------------------------------------------------
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
   
 
  app.use(notFound);
